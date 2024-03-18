67cb7d84172d9037bf37b40ebe3a6adfc0422ec1
prubioamzubiri <prubioam@zubirimanteo.com>
2024-03-18 11:17:07 +0000

    Añadir botón de eliminación de mensajes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import 'firebase/analytics';
+//import 'firebase/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // your config
+  apiKey: "AIzaSyAgee9r3urR8YvAeT5tjXl5UF8N8Ju7LwE",
+  authDomain: "chat-firebase-9235e.firebaseapp.com",
+  projectId: "chat-firebase-9235e",
+  storageBucket: "chat-firebase-9235e.appspot.com",
+  messagingSenderId: "760646584713",
+  appId: "1:760646584713:web:010ea1bbb2207eed37f958"
 })
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
+//const analytics = firebase.analytics();
 
 
 function App() {
@@ -44,10 +49,19 @@ function SignIn() {
     auth.signInWithPopup(provider);
   }
 
+  const signInWithEmail = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
   return (
     <>
+      <h1>Chat Firebase</h1>
+      <button className="sign-in" onClick={signInWithEmail}>Sign in with Email</button>
+      <br></br>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <br></br>
+      <a href="https://chat-firebase-9235e.web.app/">Registarse</a>
     </>
   )
 
@@ -105,6 +119,10 @@ function ChatRoom() {
   </>)
 }
 
+function deleteMessage(message) {
+  const messageRef = firestore.collection('messages').doc(message.id);
+  messageRef.delete();
+}
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
@@ -115,6 +133,7 @@ function ChatMessage(props) {
     <div className={`message ${messageClass}`}>
       <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
       <p>{text}</p>
+      <button id="delete_message" onClick={() => deleteMessage(props.message)}><span role="img" aria-label="delete">🗑️</span></button>
     </div>
   </>)
 }
